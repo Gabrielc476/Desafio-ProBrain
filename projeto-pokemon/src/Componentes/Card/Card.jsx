79b612bca41e522ad6c3527a3aaef494d147b7d4
Gabrielc476 <gabrielc.cavalcante@hotmail.com>
2025-01-18 00:13:01 -0300

    cards prototipo

--- projeto-pokemon/src/Componentes/Card/Card.jsx
+++ projeto-pokemon/src/Componentes/Card/Card.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-
+import "./Card.css"
 const Card = ({pokemon}) => {
   return (
     <div className='card'>
-        <div>
+        <div className='cardImagem'>
             <img src={pokemon.images.small} alt="" />
         </div>
-        <div>
-            <div>
+        <div className='cardInfo'>
+            <div className='cardDados'>
                 {pokemon.name}
                 {pokemon.types.map((tipo) => (
                     <p>{tipo}</p>
                 ))}
             </div>
-            <div>
+            <div className='cardDados'>
             {pokemon.types.map((tipo) => (
-                    <p>{tipo}</p>
+                    <p>Tipo {tipo}</p>
                 ))}
             {pokemon.rarity}
             </div>
